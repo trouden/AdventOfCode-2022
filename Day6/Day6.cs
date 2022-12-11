f98b588ad0c3a675f2b4f38a7d21fed88fc74d23
trouden <gilles.alluyn@gmail.com>
2022-12-11 22:22:35 +0100

    Solve day 6

---
+++ Day6/Day6.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode._Shared;
+
+namespace AdventOfCode.Day6;
+
+public class Day6 : BaseDayWithPuzzleInput
+{
+    public override int Day => 6;
+    public override string DayName => "Day 6: Tuning Trouble";
+
+    public override async Task SolveChallenge1()
+    {
+        var puzzleInput = (await GetPuzzleInput()).Single();
+
+        const int windowSize = 4;
+
+        var i = windowSize;
+
+        var found = false;
+
+        foreach (var window in puzzleInput.ToCharArray().Window(windowSize))
+        {
+            if (window.Distinct().Count() == windowSize)
+            {
+                found = true;
+                break;
+            }
+            i++;
+        }
+
+        Console.WriteLine(found
+            ? $"Challenge 1: the start sequence is after this many characters: {i}"
+            : "Challenge 1: no start sequence found.");
+    }
+
+    public override async Task SolveChallenge2()
+    {
+        var puzzleInput = (await GetPuzzleInput()).Single();
+
+        const int windowSize = 14;
+
+        var i = windowSize;
+
+        var found = false;
+
+        foreach (var window in puzzleInput.ToCharArray().Window(windowSize))
+        {
+            if (window.Distinct().Count() == windowSize)
+            {
+                found = true;
+                break;
+            }
+            i++;
+        }
+
+        Console.WriteLine(found
+            ? $"Challenge 2: the message sequence is after this many characters: {i}"
+            : "Challenge 2: no message sequence found.");
+    }
+}
--- _Shared/IEnumerableExtensions.cs
+++ _Shared/IEnumerableExtensions.cs
@@ -3,4 +3,22 @@
 public static class IEnumerableExtensions
 {
     public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) => enumerable == null || !enumerable.Any();
+
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach(var item in enumerable) action(item);
+    }
+
+    public static IEnumerable<T[]> Window<T>(this IEnumerable<T> enumerable, int windowSize)
+    {
+        var array = enumerable.ToArray();
+
+        if (windowSize > array.Length)
+            throw new ArgumentException(windowSize.ToString(), nameof(windowSize), null);
+
+        for (var i = 0; i + windowSize <= array.Length; i++)
+        {
+            yield return array[i..(i + windowSize)];
+        }
+    }
 }
